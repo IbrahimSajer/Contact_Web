eb932f66428f0d7675a60a15cc1078f1af4f765e
ibrahim alsajer <ibrahim.alsajer@gmail.com>
2019-07-29 15:25:04 +0200

    inital Commit

---
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContactWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/Scripts/jquery.datatables.min.js",
+                        "~/Scripts/datatables.bootstrap.min.js",
+                        "~/Scripts/datatables.colReorder.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                    "~/Content/jquery.datatables.min.css",
+                    "~/Content/jquery.datatables_themeroller.css",
+                    "~/Content/datatables.bootstrap.min.css",
+                    "~/Content/colReorder.bootstrap.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
